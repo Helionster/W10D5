1a107d46e18327ca87ddde308443b4b42dede2a6
Omar <ohammad@gwu.edu>
2023-10-27 13:44:39 -0400

    finished Phase 2

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,9 +1,16 @@
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./GalleryNavigation";
+import GalleryView from "./components/GalleryView";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <GalleryNavigation galleries = {harvardArt.records}/>
+    <div className="page-wrapper">
+      <GalleryNavigation galleries = {harvardArt.records}/>
+      <Route path='/galleries/:galleryid'>
+        <GalleryView galleries = {harvardArt.records}/>
+      </Route>
+    </div>
   );
 };
 
