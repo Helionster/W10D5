c265e7762ac12852e25bc9ae057db3a90e41b8e1
Jason <jasonzhang501@gmail.com>
2023-10-27 16:13:00 -0400

    Finished P1

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -3,7 +3,6 @@ import GalleryNavigation from "./GalleryNavigation";
 
 function App() {
   return (
-    // <h1>Hello from App</h1>
     <GalleryNavigation galleries = {harvardArt.records}/>
   );
 };
--- art-museum/src/GalleryNavigation.jsx
+++ art-museum/src/GalleryNavigation.jsx
@@ -1,8 +1,29 @@
 import {NavLink} from "react-router-dom";
+import harvardArt from "./data/harvardArt";
+import "./components/GalleryNavigation/GalleryNavigation.css";
 
 const GalleryNavigation = props => {
+
+    const gallariesLinks = props.galleries.map((gallery) => {
+        return (
+        <NavLink to={`/galleries/${gallery.galleryid}`}>
+            <li>
+                {gallery.name}
+            </li>
+        </NavLink>)
+    });
+
     return (
-        console.log(props.galleries)
+        <nav>
+            <h1>Galleries</h1>
+            <NavLink to="/">
+                <h1>Home Page</h1>
+            </NavLink> 
+
+            <ul>
+                {gallariesLinks}
+            </ul>
+        </nav>
     )
 };
 
